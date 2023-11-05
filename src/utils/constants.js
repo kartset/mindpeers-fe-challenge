dba0afa5c4158a1dba6f35c00c3c7ffcc505f0b0
Kartik Setia <kartset10@gmail.com>
2023-11-05 20:11:29 +0530

    for pie chart data

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const data = [
+    { name: "InActive", value: 7, color: "#82bc0000" },
+    { name: "Active", value: 2, color: "#A096ED" },
+    { name: "InActive", value: 6, color: "#82bc0000" },
+    { name: "Active", value: 6, color:"#A096ED" },
+    { name: "InActive", value: 3, color: "#82bc0000"},
+  ];
